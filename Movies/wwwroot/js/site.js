76cf1ef3ff6c6c6b3b3a1c88e1f5111e0726738b
Ethan Renner <eprenner@win.cis.ksu.edu>
2019-11-21 14:37:34 -0600

    In class clientside acitivities.

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var movieEntries = document.getElementsByClassName("movie-entry");
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var i, entry;
+
+    var searchTerm = document.getElementById("search").value;
+
+    var mpaa = [];
+    var mpaaCheckBoxes = document.getElementsByName("mpaa");
+    for (var j = 0; j < mpaaCheckBoxes.length; j++) {
+        if (mpaaCheckBoxes[j].checked) {
+            mpaa.push(mpaaCheckBoxes[j].value);
+        }
+    }
+    var minIMDB = document.getElementById("minIMDB").value;
+
+    for (i = 0; i < movieEntries.length; i++) {
+        entry = movieEntries[i];
+        entry.style.display = "block";
+
+        if (searchTerm && !entry.dataset.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            entry.style.display = "none";
+        }
+
+        if (mpaa.length > 0 && !mpaa.includes(entry.dataset.mpaa)) {
+            entry.style.display = "none";
+        }
+
+        if (minIMDB) {
+            if (!entry.dataset.imdb || parseFloat(minIMDB) > parseFloat(entry.dataset.imdb)) {
+                entry.style.display = "none";
+            }
+        }
+    }
+});
